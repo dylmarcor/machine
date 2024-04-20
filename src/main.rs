11c9f4053fb362ddb523f1ccc5b4d06b207204c8
dylmarcor <dylancorbett@icloud.com>
2024-04-20 10:20:50 -0700

    add formula for rpm calculation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let x: f32 = 90.0;
+    let y: f32 = 1.5;
+    println!("rpm is {:?}", rpm(x, y));
+}
+
+// Function for calculating RPM - Rotations Per Minute
+fn rpm(sfpm: f32, diameter: f32) -> f32 {
+    3.82 * sfpm / diameter
+}
+
+// Function for calculating SFPM - Surface Feet Per Minute
+fn sfpm() {
+    
 }
