b08d8b48a40e8a3760b7c740d5c3338d745ae64e
dylmarcor <dylancorbett@icloud.com>
2024-04-27 09:12:43 -0700

    update help function

--- src/help.rs
+++ src/help.rs
@@ -5,17 +5,23 @@ pub mod help {
     pub fn display_help() {
         println!("Which function would you like help with?");
 
-        let mut input = String::new();
-
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Could not read user input");
-
         loop {
+            let mut input = String::new();
+
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Could not read user input");
+
+            let input = input.trim(); 
+
             if input == "rpm" {
-                println!("You must enter the SFPM and the Diameter of the material in inches, in that order.\n\n
+                println!("You must enter the SFPM and the Diameter of the material in inches, in that order.\n
                         Usage: rpm <SFPM> <Diameter>\n
                         Example: rpm 90.0 1.5\n");
+            }
+
+            if input == "q" || input == "quit" {
+                break;
             }
         }
     }
